5047344e473c5ac1b7835755b6c163309f7078c9
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-08-30 16:10:41 -0300

    First Commit

--- databaseStrategy/database.js
+++ databaseStrategy/database.js
@@ -3,8 +3,6 @@ import pg from "pg";
 dotenv.config();
 
 const { Pool } = pg;
-const connection = new Pool({
+export const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
-
-export default connection;
