b6da7e6f068e162ca1bc8f0825aabcb03f4576cb
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-01 20:07:10 -0300

    Project done without bonuses

--- dist/routers/indexRouter.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import cardsRouter from "./cardsRouter.js";
-import transactionRouter from "./transactionRouter.js";
-var indexRouter = Router();
-indexRouter.use([cardsRouter, transactionRouter]);
-export default indexRouter;
--- dist/routers/transactionRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { apiKeyValidator } from "../middlewares/apiKeyValidator.js";
-import { schemasMiddleware } from "../middlewares/schemasMiddleware.js";
-import { rechargeSchema, paymentSchema } from "../schemas/transactionsSchemas.js";
-import { rechargeController, paymentController } from "../controllers/transactionsController.js";
-var transactionRouter = Router();
-transactionRouter.post("/recharge", apiKeyValidator, schemasMiddleware(rechargeSchema), rechargeController);
-transactionRouter.post("/payment", schemasMiddleware(paymentSchema), paymentController);
-export default transactionRouter;
