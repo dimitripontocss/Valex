7663c6fa9b1804fec3cecbd03c6e1b9a85c35955
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-09-05 18:20:57 -0300

    Documentaçao no ReadMe

--- src/routers/transactionRouter.ts
+++ src/routers/transactionRouter.ts
@@ -8,32 +8,7 @@ import { rechargeController,paymentController,onlinePaymentController } from "..
 const transactionRouter = Router();
 
 transactionRouter.post("/recharge",apiKeyValidator, schemasMiddleware(rechargeSchema), rechargeController);
-//Header
-//Com x-api-key
-//Body
-// {
-//     "cardId": 2,
-//     "amount": 200
-// }
-
 transactionRouter.post("/payment", schemasMiddleware(paymentSchema), paymentController);
-//Body
-// {
-//   "cardId": 2,
-//   "password": "1234",
-//   "businessId": 3,
-//   "amount": 50
-// }
-
 transactionRouter.post("/payment/online", schemasMiddleware(onlinePaymentSchema), onlinePaymentController);
-//Body
-// {
-//     "number": "5185-4853-9016-1072",
-//     "cardholderName": "CICLANA M MADEIRA",
-//     "expirationDate": "09/27",
-//     "securityCode": "064",
-//     "businessId": 3,
-//     "amount": 50
-// }
 
 export default transactionRouter;
